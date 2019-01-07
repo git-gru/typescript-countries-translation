8418e48d2df9db67c67d69db22b3daff7bfe6219
Felonius Gru <new-comet-dev@outlook.com>
2019-01-07 11:10:38 +0400

    add getTranslations function

---
+++ functions/country.ts
@@ -0,0 +1,13 @@
+import { ICountryModel } from '../models/country';
+
+export const getTranslations = (translation_key: string, countries: ICountryModel[]) => {
+    return countries.map(c => {
+        var translations = c.translations;
+
+        if (translations[translation_key] && translations[translation_key].official) {
+            return translations[translation_key].official;
+        } else {
+            return `Translation for ${translation_key} is not existing for ${c.flag}`;
+        }
+    });
+}
